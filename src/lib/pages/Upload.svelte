be342cbceee4c2b1577889fe37eb439b8d271b29
Siva <sivaprasada2004@gmail.com>
2026-05-16 12:36:37 +0530

    video file inputting done

---
+++ src/lib/pages/Upload.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/core";
+    let name = $state("");
+
+    //uploading file
+    let videoFile = $state<File | null>(null);
+    let wordPerFrame = $state<number>(3);
+    let videUrl = $state<string>("");
+
+    function handleFileUpload(event: Event) {
+        const input = event.target as HTMLInputElement;
+        const file = input.files?.[0];
+        if (file) {
+            videoFile = file;
+            videUrl = URL.createObjectURL(file);
+        }
+    }
+    function handelWordsPerFrame(event: Event) {
+        const input = event.target as HTMLInputElement;
+        wordPerFrame = Number(input.value);
+    }
+</script>
+
+<main class="cont">
+    <h1>Welcome to Opensub</h1>
+</main>
---
+++ src/lib/player/VideoPlayer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { onDestroy } from "svelte";
+    import { videoPath } from "../store.js";
+
+    let videoUrl = "";
+    const unsubscribe = videoPath.subscribe((value) => {
+        videoUrl = value;
+    });
+
+    onDestroy(unsubscribe);
+</script>
+
+<!-- svelte-ignore a11y_media_has_caption -->
+<video src={videoUrl}></video>
