f51018cbff2d1daf9f7c9f97b6353307591f6652
Siva <sivaprasada2004@gmail.com>
2026-05-19 13:43:07 +0530

    onmount function added

--- src/routes/loading/+page.svelte
+++ src/routes/loading/+page.svelte
@@ -1,23 +1,39 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import { goto } from "$app/navigation";
     import { subtitle, videoPath, type Subtitle } from "$lib/store";
     import { invoke } from "@tauri-apps/api/core";
+    import { get } from "svelte/store";
+
     type Pyres = {
         status: string;
         message: Subtitle[] | string;
     };
+
     async function generateSubtitle(videoUrl: string, count: number) {
         const sub = await invoke<Pyres>("run_python", {
             name: videoUrl,
             count: count,
         });
-        if (sub.status == "ok") {
+
+        if (sub.status === "ok") {
+            subtitle.set(sub.message as Subtitle[]);
             await goto("/editor");
         } else {
-            alert("some error has occured click ok to go back");
-            //need to write the go back function
+            alert("Some error has occurred");
+            await goto("/");
         }
     }
+
+    onMount(async () => {
+        const video = get(videoPath);
+        if (!video) {
+            await goto("/");
+            return;
+        }
+
+        await generateSubtitle(video, 3);
+    });
 </script>
 
 <main>
